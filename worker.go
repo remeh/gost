996d64f7abd3b3e3c9ee27dcbd27a14881715694
remeh <remeh@remeh.fr>
2014-08-20 14:13:36 +0200

    Working NSQWorker to have a simple LoggerWorker

--- worker.go
+++ worker.go
@@ -3,6 +3,7 @@ package gost
 import (
     "errors"
     "fmt"
+    "log"
     "github.com/bitly/go-nsq"
 )
 
@@ -11,32 +12,47 @@ import (
 // in the storage system to be available when
 // dest clients come for data.
 type Worker interface {
-    Init(gost Gost, target string, action string, config nsq.Config) (*nsq.Consumer, error)
+    Init(gost Gost, target string, action string) error
     Stop()
     Run(Task) (Task, []byte)    // Executes the supplied task given by the broadcaster
+    Store(Task, []byte) error
 }
 
 // A simple worker is a basic implementation using the NSQ broadcaster.
-type SimpleWorker struct {
+type NSQWorker struct {
     gost Gost
+    consumer    *nsq.Consumer   // the NSQ consumer listening for work
+    taskChannel chan Task
 }
 
-// TODO nsq config should be abstracted
-func (w *SimpleWorker) Init(gost Gost, target string, action string, config nsq.Config) (*nsq.Consumer, error) {
+// TODO nsq config should be abstracted and given in a parameter
+func (w *NSQWorker) Init(gost Gost, target string, action string) error {
     w.gost = gost
 
     // Creates the new consumer
-    consumer, err := nsq.NewConsumer(target, action, &config)
+    consumer, err := nsq.NewConsumer(target, action, nsq.NewConfig())
+    w.consumer = consumer
 
     if err != nil {
-        return nil, err
+        return err
     }
 
-    return consumer, nil
+    // The worker handle the message reception
+    w.consumer.AddHandler(w)
+
+    log.Println("[WORKER] [logger] Handler attached.")
+
+    // Connects the consumer to the broadcaster
+    w.Connect(consumer, gost)
+
+    // Inits the communication chan
+    w.taskChannel = make(chan Task)
+
+    return nil
 }
 
 // Connects with NSQ to receive task to do.
-func (w *SimpleWorker) Connect(consumer *nsq.Consumer, gost Gost) error {
+func (w *NSQWorker) Connect(consumer *nsq.Consumer, gost Gost) error {
     config := gost.GetConfig()
 
     // Use the config to know to which address
@@ -50,32 +66,31 @@ func (w *SimpleWorker) Connect(consumer *nsq.Consumer, gost Gost) error {
     }
 
     if err != nil {
-        fmt.Println("[WORKER] [writer] ERROR - Unable to connect the PrinterWorker : ")
+        fmt.Println("[WORKER] [writer] ERROR - Unable to connect the LoggerWorker : ")
         fmt.Printf("[WORKER] [writer] ERROR - %s\n", err)
-        return errors.New("Unable to start the PrinterWorker")
+        return errors.New("Unable to start the LoggerWorker")
     }
     return nil
+}
+
+func (w *NSQWorker) Store(task Task, data []byte) error {
+    // Store the result
+    err := w.gost.GetStorage().Store(task.GetId(), data)
+    return err
 }
 
 // Handles the message coming from NSQ, unserialize
 // it to create a task, execute it in the worker and
 // finally call the gost storage to store the result.
-func (w *PrinterWorker) HandleMessage(m *nsq.Message) error {
+func (w *NSQWorker) HandleMessage(m *nsq.Message) error {
     task := UnserializeSimpleTask(m.Body)
     if task == nil {
         return errors.New(fmt.Sprintf("Unable to unserialize the task : %s", m.Body))
     }
 
-    // Retrieve the result of the task execution
-    returnedTask, result := w.Run(task)
-
-    // Store the result
-    err := w.gost.GetStorage().Store(returnedTask.GetId(), result)
-    if err != nil {
-        return err
-    }
+    // Distribute it to the implementation
+    w.taskChannel <- task
 
     m.Finish()
     return nil
 }
-
